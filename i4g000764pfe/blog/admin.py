c604687e7e893de47e03d8759d48375d7060b212
Reza <privyreza@gmail.com>
2022-06-24 23:10:26 +0200

    Added templates and urls

--- i4g000764pfe/blog/admin.py
+++ i4g000764pfe/blog/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from models import Post
 
-# Register your models here.
+# Option 1 - Basic
+admin.site.register(Post)  
--- i4g000764pfe/blog/views.py
+++ i4g000764pfe/blog/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from msilib.schema import ListView
+
+from django.urls import reverse_lazy
+from models import Post
 
 # Create your views here.
+class PostListView(ListView):
+    model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
+
+class PostDeleteView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
